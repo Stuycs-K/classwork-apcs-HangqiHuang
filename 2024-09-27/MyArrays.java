98d2810699f00db78eabfe893ef5bd2ba29e9a08
HangqiHuang <hhuang60@stuy.edu>
2024-09-27 09:03:17 -0500

    arrayToString() works

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,17 +1,18 @@
-public class MyArrays.java{
+public class MyArrays{
   public static void main(String args[]){
-
+    System.out.print(arrayToString(new int[] {1,2,3,4,5,6,7,8,9}));
   }
 
-public static String arrayToString(int[] nums){
-  String result = "[";
-  if (nums.length != 0){
-    for (int i = 0; i < nums.length; i++){
-      if (i != nums.length - 1){
-        result += nums[i] + ", ";
+  public static String arrayToString(int[] nums){
+    String result = "[";
+    if (nums.length != 0){
+      for (int i = 0; i < nums.length; i++){
+        result += nums[i];
+        if (i != nums.length - 1){
+          result += ", ";
+        }
       }
     }
+    return result += "]";
   }
-  return result+="]";
-}
 }
