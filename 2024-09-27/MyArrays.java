9a6d07c430f9964337d9caf07ad9b3622e4def0c
HangqiHuang <hhuang60@stuy.edu>
2024-09-27 09:15:07 -0500

    first test case done

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,6 +1,15 @@
 public class MyArrays{
-  public static void main(String args[]){
-    System.out.print(arrayToString(new int[] {1,2,3,4,5,6,7,8,9}));
+  public static void main(String[] args){
+    System.out.println("Testing returnCopy: ");
+    int[] originalAry = new int[] {1, 5, 54, 2, 5, 778, 0};
+    int [] newAry = returnCopy(originalAry);
+    boolean returnCopyWorks true;
+    for (int i = 0; i < originalAry; i++){
+      if(originalAry[i] != newAry[i]){
+        return returnCopyWorks;
+      }
+    }
+    System.out.println(returnCopyWorks && (originalAry != newAry));
   }
 
   public static String arrayToString(int[] nums){
