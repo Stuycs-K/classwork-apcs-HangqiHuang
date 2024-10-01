fd96508ddfe95cd569f6bdb014c30c2930e1d2b8
HangqiHuang <hhuang60@stuy.edu>
2024-09-30 20:41:03 -0400

    added test cases for sum

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -3,16 +3,25 @@ public class ArrayMethods{
     public static void main(String[] args){
       int[] firstArray1D = new int[] {1, 2, 3, 4, 5};
       int[] secondArray1D = new int [] {};
+      System.out.println("Testing 1D array");
       System.out.println(aryToString(firstArray1D));
       System.out.println(aryToString(secondArray1D));
       int[][] squareArray2D = new int[][] {{1,2,3}, {4,5,6}, {7,8,9}};
-      int[][] emptyArray2D = new int[3][];
+      int[][] emptyArray2D = new int[3][];//because I haven't preset the column, the array holds null
       int[][] rectangularArray2D = new int[3][4];
       int[][] raggedArray2D = {{1}, {1, 2}, {1, 2, 3}};
+      System.out.println("Testing 2D aryToString");
       System.out.println(aryToString(squareArray2D));
       System.out.println(aryToString(emptyArray2D));
       System.out.println(aryToString(rectangularArray2D));
       System.out.println(aryToString(raggedArray2D));
+      System.out.println("Testing sum");
+      System.out.println("Expected 45 vs tested: " + arr2DSum(squareArray2D));
+      System.out.println("Expected 0 vs tested: " + arr2DSum(emptyArray2D));
+      System.out.println("Expected 0 vs tested: " + arr2DSum(rectangularArray2D));
+      System.out.println("Expected 10 vs tested: " + arr2DSum(raggedArray2D));
+      rectangularArray2D[1] = new int[] {1, 2, 3, 4};
+      System.out.println("Expected 10 vs tested: " + arr2DSum(rectangularArray2D));
     }
   
     public static String aryToString(int[] nums){
@@ -40,5 +49,8 @@ public class ArrayMethods{
         }
         result += "}";
         return result;
+}
+public static int arr2DSum(int[][] nums){
+    return 0;
 }
 }
