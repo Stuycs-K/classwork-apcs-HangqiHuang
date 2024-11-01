0f20fa4a03d74dc74b04c739f4b742c2cbff8aba
HangqiHuang <hhuang60@stuy.edu>
2024-10-31 22:23:32 -0400

    how do I pass the sum outside try{}?

--- 2024-10-31-Advent/TriangleTester.java
+++ 2024-10-31-Advent/TriangleTester.java
@@ -4,16 +4,30 @@ import java.io.FileNotFoundException;
 
 public class TriangleTester{
   public static int countTriangle(String filename){
-    return 0;
+    try{
+      int sum = 0;
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+      while(input.hasNextLine()){
+        if(validTriangle(input.nextLine())){
+          sum++;
+        }
+        input.close();
+        return sum;
+      }
+    }catch(FileNotFoundException e){
+      System.out.println("File not found");
+    }
   }
   public static boolean validTriangle(String sideLengths){
     Scanner sides = new Scanner(sideLengths);
-    int a = Integer.parseInt(sides.next());
-    int b = Integer.parseInt(sides.next());
-    int c = Integer.parseInt(sides.next());
+    int a = sides.nextInt();
+    int b = sides.nextInt();
+    int c = sides.nextInt();
     return (a + b > c && a + c > b && b + c > a);
   }
   public static void main(String[] args){
     System.out.println(validTriangle("5 5 2"));
+    System.out.println(countTriangle("inputTri.txt"));
   }
 }
