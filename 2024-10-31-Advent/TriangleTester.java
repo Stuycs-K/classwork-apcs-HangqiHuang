c90a2bbcf4ae0d3a74896d710ebce70ab513d6d3
HangqiHuang <hhuang60@stuy.edu>
2024-11-01 11:58:37 -0400

    moved sum outside try{}; countTriangle works now

--- 2024-10-31-Advent/TriangleTester.java
+++ 2024-10-31-Advent/TriangleTester.java
@@ -4,20 +4,19 @@ import java.io.FileNotFoundException;
 
 public class TriangleTester{
   public static int countTriangle(String filename){
+    int sum = 0;
     try{
-      int sum = 0;
       File file = new File(filename);
       Scanner input = new Scanner(file);
       while(input.hasNextLine()){
         if(validTriangle(input.nextLine())){
           sum++;
         }
-        input.close();
-        return sum;
       }
     }catch(FileNotFoundException e){
       System.out.println("File not found");
     }
+    return sum;
   }
   public static boolean validTriangle(String sideLengths){
     Scanner sides = new Scanner(sideLengths);
@@ -27,7 +26,6 @@ public class TriangleTester{
     return (a + b > c && a + c > b && b + c > a);
   }
   public static void main(String[] args){
-    System.out.println(validTriangle("5 5 2"));
     System.out.println(countTriangle("inputTri.txt"));
   }
 }
