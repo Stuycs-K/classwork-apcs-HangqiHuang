710c6466fd15648619930c619a16c58d37d948c1
HangqiHuang <hhuang60@stuy.edu>
2024-11-07 19:21:49 -0500

    finished answering all questions

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,3 +1,6 @@
+//When the getName() is overrided, the getName() in the Bird class runs the new getName() but the animal class still runs the old getName().
+
+ //Bird = Animal don't work because Animal doesn't fit into Bird. Bird is-a Animal, but Animal isn't-a Bird.
 public class Driver{
   public static void main(String[] args){
     Animal Fred = new Animal("Baaaaa", 3, "Fred");
@@ -7,7 +10,11 @@ public class Driver{
 
     Animal a1 = new Animal("meow", 3, "a1");
     Bird b1 = new Bird("woof", 2, "b1", 2.0, "light blue");
-    //Bird b2 = new Animal("meow", 3, "b2"); This one don't work
+    //Bird b2 = new Animal("meow", 3, "b2");
     Animal a2 = new Bird("woof", 2, "a2", 2.0, "light blue");
+    a1.speak();
+    b1.speak();
+    a2.speak();
+    //even though a2 is an Animal, the speak() method is the one written in Bird because a2 technically is a Bird, so it uses the Bird.speak() method.
   }
 }
