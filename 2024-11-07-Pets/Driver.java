095db870b6fa6a89d5f8e8938c7a57e41a32343e
HangqiHuang <hhuang60@stuy.edu>
2024-11-07 19:15:21 -0500

    copied files over

---
+++ 2024-11-07-Pets/Driver.java
@@ -0,0 +1,13 @@
+public class Driver{
+  public static void main(String[] args){
+    Animal Fred = new Animal("Baaaaa", 3, "Fred");
+    Fred.speak();
+    Bird Birdo = new Bird("Squaaaak", 2, "Birdo", 4.0, "pink");
+    Birdo.speak();
+
+    Animal a1 = new Animal("meow", 3, "a1");
+    Bird b1 = new Bird("woof", 2, "b1", 2.0, "light blue");
+    //Bird b2 = new Animal("meow", 3, "b2"); This one don't work
+    Animal a2 = new Bird("woof", 2, "a2", 2.0, "light blue");
+  }
+}
