2c7d8ba7ed8a982a8da036018874ed53d429af1a
HangqiHuang <hhuang60@stuy.edu>
2024-11-14 19:39:21 -0500

    done

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -31,4 +31,30 @@ public class ArrayListPractice{
     }
     return result;
   }
+  public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
+  //return a new ArrayList that has all values of a and b in alternating order that is:
+  //a[0], b[0], a[1], b[1]...
+  //If one list is longer than the other, just attach the remaining values to the end.
+  if(a.size() < b.size()){
+    ArrayList<String> temp = a;
+    a = b;
+    b = temp;
+  }
+    ArrayList<String> result = new ArrayList<String>(a.size() + b.size());
+    for(int i = 0; i < b.size(); i++){
+        result.add(a.get(i));
+        result.add(b.get(i));
+    }
+    if(b.size() != 0){
+      for(int i = b.size(); i < a.size(); i++){
+        result.add(a.get(i));
+      }
+    }
+    else{
+      for(int i = 0; i < a.size(); i++){
+        result.add(a.get(i));
+      }
+    }
+    return result;
+  }
 }
--- 2024-11-14-ArrayList/Driver.java
+++ 2024-11-14-ArrayList/Driver.java
@@ -5,22 +5,31 @@ public class Driver{
     ArrayList<String> test1 = ArrayListPractice.createRandomArray(20);
     ArrayList<String> test2 = ArrayListPractice.createRandomArray(0);
     ArrayList<String> test3 = ArrayListPractice.createRandomArray(20);
+    ArrayList<String> test4 = ArrayListPractice.createRandomArray(10);
     for(int i = 0; i < 20; i++){
       if((int) (Math.random() * 11) > 8){
-        test3.add(null);
+        test3.set(i, null);
       }
     }
     System.out.println(test1);
     ArrayListPractice.replaceEmpty(test1);
-    System.out.println(test1);
+    System.out.println("Test1: " + test1);
     System.out.println(test2);
     ArrayListPractice.replaceEmpty(test2);
-    System.out.println(test2);
+    System.out.println("Test2: " + test2);
     System.out.println(test3);
     ArrayListPractice.replaceEmpty(test3);
-    System.out.println(test3);
+    System.out.println("Test3: " + test3);
     System.out.println(ArrayListPractice.makeReversedList(test1));
     System.out.println(ArrayListPractice.makeReversedList(test2));
     System.out.println(ArrayListPractice.makeReversedList(test3));
+    System.out.println("Test4" + test4);
+    System.out.println("Test1 + 4: " + ArrayListPractice.mixLists(test1, test4));
+    System.out.println(ArrayListPractice.mixLists(test1, test4).equals(ArrayListPractice.mixLists(test4, test1)));
+    System.out.println("Test1 + 2: " + ArrayListPractice.mixLists(test1, test2));
+    System.out.println("Test1 + 3: " + ArrayListPractice.mixLists(test1, test3));
+    System.out.println("Test2 + 3: " + ArrayListPractice.mixLists(test2, test3));
+    System.out.println("Test2 + 4: " + ArrayListPractice.mixLists(test2, test4));
+    System.out.println("Test3 + 4: " + ArrayListPractice.mixLists(test3, test4));
   }
 }
