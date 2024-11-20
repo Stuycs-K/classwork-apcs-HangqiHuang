cb17b3b3760d83e299595e5f23855a76229b80aa
HangqiHuang <hhuang60@stuy.edu>
2024-11-19 21:52:40 -0500

    day 2 part 1 done

--- 2024-11-18-advent2016/Day2.java
+++ 2024-11-18-advent2016/Day2.java
@@ -15,13 +15,19 @@ public class Day2{
   }
   public static void bathroomCodeA(String filename){
     Scanner input = parseFile(filename);
-    int current = 5, direction = 0, index = 4;
-    int[] keypad = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int[] offset = {-3, -1, 3, 1};
+    int[][] keypad = {
+      {1, 2, 3}, {4, 5, 6}, {7, 8, 9}
+    };
+    int current = 5, row = 1, column = 1, direction = 0;
+    int[][] offset = {
+      {-1, 0}, {0, -1}, {1, 0}, {0, 1}
+    };
     while(input.hasNextLine()){
-      String line= input.nextLine();
+      String line = input.nextLine();
+      // System.out.println(line);
       for(int i = 0; i < line.length(); i++){
         String instruction = line.substring(i, i + 1);
+        // System.out.println(instruction);
         if(instruction.equals("U")){
           direction = 0;
         }
@@ -34,9 +40,13 @@ public class Day2{
         else{
           direction = 3;
         }
-        if(index + offset[direction] >= 0 && index + offset[direction] < 9){
-          index += offset[direction];
-          current = keypad[index];
+        if(row + offset[direction][0] >= 0 && row + offset[direction][0] < 3){
+          if(column + offset[direction][1] >= 0 && column + + offset[direction][1] < 3){
+            row += offset[direction][0];
+            column += offset[direction][1];
+            current = keypad[row][column];
+            // System.out.println(current);
+          }
         }
       }
       System.out.println(current);
