5781156059808c685f4dc06bcb16c90918b42762
HangqiHuang <hhuang60@stuy.edu>
2024-12-03 19:30:40 -0500

    done

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,13 +1,43 @@
 public class ColorDemo{
+  //give length of animation when running
+  public static final String CLEAR_SCREEN = "\u001b[2J";
+  public static final String DEFAULT = "\u001b[0m";
   public static void main(String[] args){
-    for(int r = 0; r < 256; r+=32){
-  for(int g = 0; g <= 256; g+=32){
-    for(int b = 0; b <= 256; b+=32){
-      System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m.");
+    int length = Integer.parseInt(args[0]);
+    for(int i = 0; i < length; i++){
+      circle(i);
+      sleep(60);
+      System.out.print(CLEAR_SCREEN);
     }
+    circle(length);
   }
-    System.out.println();
-}
 
+  public static void circle(int movement){
+    String color = "\u001b[41m";
+    String displacement = "";
+    for(int i = 0; i < movement; i++){
+      displacement += " ";
+    }
+    System.out.println(displacement + "    " + color + "   " + DEFAULT + "    ");
+    System.out.println(displacement + "  " + color + "  " + DEFAULT + "   " + color + "  " + DEFAULT);
+    for(int i = 0; i < 2; i++){
+      System.out.println(displacement + " " + color + " " + DEFAULT + "       " + color + " " + DEFAULT);
+    }
+    for(int i = 0; i < 3; i++){
+      System.out.println(displacement + color + " " + DEFAULT + "         " + color + " " + DEFAULT);
+    }
+    for(int i = 0; i < 2; i++){
+      System.out.println(displacement + " " + color + " " + DEFAULT + "       " + color + " " + DEFAULT);
+    }
+    System.out.println(displacement + "  " + color + "  " + DEFAULT + "   " + color + "  " + DEFAULT);
+    System.out.println(displacement + "    " + color + "   " + DEFAULT + "    ");
+    System.out.print(DEFAULT);
+  }
+  public static void sleep(int milli){
+    try{
+      Thread.sleep(milli);
+    }catch(Exception e){
+
+    }
   }
 }
