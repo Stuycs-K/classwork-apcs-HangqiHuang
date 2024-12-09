addbd9a028e7e936d32c36024011f7b67a136136
HangqiHuang <hhuang60@stuy.edu>
2024-12-09 10:12:21 -0500

    working on the actual gameplay rn

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class Game{
   public static void main(String[] args){
     Scanner userInput = new Scanner(System.in);
     System.out.println("Enter username");
     String userName = userInput.nextLine();
-    System.out.println("Hello " + userName);
+    System.out.println("Hello " + userName + ". You are a Scholar and your enemy is a codeWarrior. Fight until one of you dies. Type \"a\" to attack, \"sp\" to perform your special attack, \"su\" to support yourself, or \"quit\" to end the game.");
+    Scholar player = new Scholar(userName);
+    CodeWarrior enemy = new CodeWarrior("Enemy", 100);
+    // System.out.println("Hello " + userName + ". Choose the type of Adventurer you want to be. Enter (CodeWarrior/Scholar)");
+    // String type = userInput.nextLine();
+    // while(type.equals("CodeWarrior") || type.equals("Scholar"))
+    String input = userInput.nextLine();
+    while()
   }
 }
