925a776042e89e1254ac86fefffc0518e140f7d8
HangqiHuang <hhuang60@stuy.edu>
2025-01-03 10:13:35 -0500

    working on toword

--- 2025-01-02-fibonacci/MakeWord.java
+++ 2025-01-02-fibonacci/MakeWord.java
@@ -9,8 +9,49 @@ public class MakeWord{
       }
     }
   }
+  public static String toWords(int n){
+    if(("" + n).length() == 1){
+      if(n == 0){
+        return "zero";
+      }
+      if(n == 1){
+        return "one";
+      }
+      if(n == 2){
+        return "two";
+      }
+      if(n == 3){
+        return "three";
+      }
+      if(n == 4){
+        return "four";
+      }
+      if(n == 5){
+        return "five";
+      }
+      if(n == 6){
+        return "six";
+      }
+      if(n == 7){
+        return "seven";
+      }
+      if(n == 8){
+        return "eight";
+      }
+      if(n == 9){
+        return "nine";
+      }
+    }
+    else{
+      if(("" + n).length() == 2){
+
+      }
+    }
+    return "zero";
+  }
 
   public static void main(String[] args){
     makeWords(3, "", "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    System.out.println(toWords(5));
   }
 }
